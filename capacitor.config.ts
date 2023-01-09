21ac889e59a1fef836af7d559bff3a7bd806358d
andres174 <zambrasda17@gmail.com>
2023-01-09 15:14:08 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '04AppNotificaciones',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
